387cbe782f9fa403beb4e777929365f49520eea6
jadsonmd <jadsonmd@gmail.com>
2017-11-15 22:15:08 -0200

    Cadastrar atores do sistema
    Aplicação responsável por cadastrar os atores do sistema.

---
+++ config/server.js
@@ -0,0 +1,8 @@
+/* importar o módulo do framework express */
+var express = require('express');
+
+/* iniciar o objeto do express */
+var app = express();
+
+/* exportar o objeto app */
+module.exports = app;
---
+++ routes/tomador.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var app = express();
+
+//POST (create)
+app.post('/tomador', function(req, res){
+	
+});
+
+
+//GET (ready)
+app.get('/tomador', function(req, res){
+
+});
+
+
+//GET by ID (ready)
+app.get('/tomador/:id', function(req, res){
+
+});
+
+
+//PUT by ID (update)
+app.put('/tomador/:id', function(req, res){
+
+});
+
+
+//DELETE by ID (remover)
+app.delete('/tomador/:id', function(req, res){
+
+});
